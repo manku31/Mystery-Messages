f4d7d310e62b583adb45d5a40e64f8b26d1a9069
Prashant Singh <prashantdineshsingh48@gmail.com>
2024-07-25 12:21:38 +0530

    Update middleware redirect logic

--- middleware.ts
+++ middleware.ts
@@ -6,21 +6,30 @@ export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request });
   const url = request.nextUrl;
 
-  /* Redirect to dashboard page
-   * - If user is lockedIn (token is true )
-   * - AND url is /sign-in, /sign-up, /, /verify
+  /*  If user is lockedIn
+   *  - check if url start with /sign-in, /sign-up, / or /verify
+   *    - redirect to dashboard
+   *  Else user is not lockedIn
+   *  - check if url start with /dashboard
+   *    - redirect to dashboard
    */
-  if (
-    token &&
-    (url.pathname.startsWith("/sign-in") ||
+  if (token) {
+    if (
+      url.pathname.startsWith("/sign-in") ||
       url.pathname.startsWith("/sign-up") ||
       url.pathname.startsWith("/") ||
-      url.pathname.startsWith("/verify"))
-  ) {
-    return NextResponse.redirect(new URL("/dashboard", request.url));
+      url.pathname.startsWith("/verify")
+    ) {
+      return NextResponse.redirect(new URL("/dashboard", request.url));
+    }
+  } else {
+    if (url.pathname.startsWith("/dashboard")) {
+      return NextResponse.redirect(new URL("/sign-in", request.url));
+    }
   }
 }
 
+// middleware is only used in pages
 export const config = {
   matcher: ["/sign-in", "/sign-up", "/", "/dashboard/:path*", "/verify/:path*"],
 };
