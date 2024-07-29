e9ecf038112e96d1f62343042c0dfaa9e43787a8
Prashant Singh <prashantdineshsingh48@gmail.com>
2024-07-29 21:59:07 +0530

    Setup signup page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
-import AuthProvider from "@/context/AuthProvider";
-
 import "./globals.css";
+
+import AuthProvider from "@/context/AuthProvider";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +21,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <AuthProvider>
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          {children}
+          <Toaster />
+        </body>
       </AuthProvider>
     </html>
   );
--- schemas/signUpSchema.ts
+++ schemas/signUpSchema.ts
@@ -4,10 +4,7 @@ export const usernameValidation = z
   .string()
   .min(3, "username must be atlest 3 characters")
   .max(8, "username must be no more then 8 characters")
-  .regex(
-    /[a-zA-Z][a-zA-Z0-9-_]{3,32}/gi,
-    "username must not content special characters"
-  );
+  .regex(/^[a-zA-Z0-9]+$/, "username must not content special characters");
 
 export const signUpSchema = z.object({
   username: usernameValidation,
