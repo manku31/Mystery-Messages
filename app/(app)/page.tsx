e3e9be846ebbff539adef589990c1f905fdc7b62
Prashant Singh <prashantdineshsingh48@gmail.com>
2024-08-05 11:37:56 +0530

    fix the card css

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -8,24 +8,25 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import { Separator } from "@/components/ui/separator";
 import messages from "@/messages.json";
 import Autoplay from "embla-carousel-autoplay";
 
 export default function Home() {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <main className="flex-grow flex flex-col items-center justify-center px-4 md:px-24 py-12">
         <section className="text-center mb-8 md:mb-12">
           <h1 className="text-3xl md:text-5xl font-bold">
-            Dive into the world of anonymous conevrsation
+            Dive into the world of anonymous conversation
           </h1>
           <p className="mt-3 md:mt-4 text-base md:text-lg">
-            Explor mystery message - where your identity remain a secret.
+            Explore mystery messages - where your identity remains a secret.
           </p>
         </section>
 
         <Carousel
-          className="w-full max-w-xs"
+          className="w-full max-w-2xl"
           plugins={[Autoplay({ delay: 2000 })]}
         >
           <CarouselContent className="">
@@ -33,11 +34,12 @@ export default function Home() {
               <CarouselItem key={index}>
                 <div className="p-1">
                   <Card>
-                    <CardHeader className="text-center font-semibold text-lg">
+                    <CardHeader className="font-semibold text-lg">
                       {message.title}
                     </CardHeader>
-                    <CardContent className="flex aspect-square items-center justify-center p-6">
-                      <span className="text-4xl font-semibold">
+                    <CardContent className="flex p-6 h-32 max-w-2xl">
+                      <span className="text-2xl">
+                        <p className="font-semibold">Message : </p>
                         {message.content}
                       </span>
                     </CardContent>
@@ -50,9 +52,11 @@ export default function Home() {
           <CarouselNext />
         </Carousel>
       </main>
-      <footer className="text-center p-4 md:p-10 mt-10">
-        @ 2024 Mystery Message. All rights reserved.
+
+      <footer className="text-center p-4 md:p-10">
+        <Separator />
+        <p className="mt-5">@ 2024 Mystery Message. All rights reserved.</p>
       </footer>
-    </>
+    </div>
   );
 }
